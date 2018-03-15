779f061ce6587da852b15c74a8149ed3ec386246
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-15 14:32:28 +1100

    Remove all unnecessary code or develop comments

--- lib/toy_robot_cli.sh
+++ lib/toy_robot_cli.sh
@@ -9,20 +9,12 @@ function main() {
   local robot=$(Robot_new)
   local line=""
 
-  # if [[ "$filename" ]]; then
-  #   while IFS='' read -r line || [[ -n "$line" ]]; do
-  #     robot=$(Robot_exec $robot $line)
-  #     [[ "$line" == "REPORT" ]] && echo -n "$robot\n"
-  #   done < "$filename"
-  # else
-    while read -r line && [[ "$line" != "EXIT" ]] && [[ -n "$line" ]]; do
-      robot=$(Robot_exec $robot $line)
-      if [[ "$line" == "REPORT" ]]; then
-        [[ "$input" == "/dev/stdin" && -t 0 ]] &&
-          echo -ne "$robot\n" > $(tty) ||
-          echo -n "$robot\n"
-      fi
-    done < "$input"
-    # done < /dev/stdin
-  # fi
+  while read -r line && [[ "$line" != "EXIT" ]] && [[ -n "$line" ]]; do
+    robot=$(Robot_exec $robot $line)
+    if [[ "$line" == "REPORT" ]]; then
+      [[ "$input" == "/dev/stdin" && -t 0 ]] &&
+        echo -ne "$robot\n" > $(tty) ||
+        echo -n "$robot\n"
+    fi
+  done < "$input"
 }
