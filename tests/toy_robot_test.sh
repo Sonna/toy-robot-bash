5783d664a840e29d61888d4162f32b5d51452b42
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-15 11:45:33 +1100

    Add Robot execute tests
    Add a few Robot execute `Robot_exec` tests to ensure that it correctly
    interprets the incoming string to function call and pass the arguments
    correctly to said function call. Also, Update all Robot functions to
    accept the Robot instance (which is a string of coordinates) as the
    first argument, so that they update the Robot rather than generating a
    new one each time.
    
    == Notes:
    - The biggest bug or concern that was troubling was not setting the
      global option for the Associative arrays; i.e. `declare -gA`; since
      when calling any function that relied on the array would fail because
      the array no longer exists outside of that context; i.e. within tests

--- tests/toy_robot_test.sh
+++ tests/toy_robot_test.sh
@@ -4,10 +4,92 @@ LIB_SCRIPT_PATH="$(pwd)/lib/toy_robot.sh"
 . "$LIB_SCRIPT_PATH"
 
 @test "new Robot" {
-  subject=$(Robot_new)
+  local subject=$(Robot_new)
   local values=(`echo $subject | tr ',' ' '`)
 
   [ "${values[0]}" -eq 0 ]
   [ "${values[1]}" == "0" ]
+  [ "${values[2]}" == "NORTH" ]
+}
+
+@test "Robot move" {
+  local subject=$(Robot_move $(Robot_new))
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" == "1" ]
+  [ "${values[2]}" == "NORTH" ]
+}
+
+@test "Robot MOVE" {
+  local subject=$(Robot_move $(Robot_new))
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" -eq 1 ]
+  [ "${values[2]}" == "NORTH" ]
+}
+
+@test "Robot exec MOVE" {
+  local subject=$(Robot_new)
+  local subject=$(Robot_exec $(Robot_new) MOVE)
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" -eq 1 ]
+  [ "${values[2]}" == "NORTH" ]
+}
+
+@test "Robot exec MOVE from 0,1,NORTH" {
+  local subject=$(Robot_new)
+  local subject=$(Robot_place $subject 0,1,NORTH)
+  local subject=$(Robot_exec $subject MOVE)
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" -eq 2 ]
+  [ "${values[2]}" == "NORTH" ]
+}
+
+@test "Robot exec LEFT" {
+  local subject=$(Robot_new)
+  local subject=$(Robot_exec $subject LEFT)
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" -eq 0 ]
+  [ "${values[2]}" == "WEST" ]
+}
+
+@test "Robot exec RIGHT" {
+  local subject=$(Robot_new)
+  local subject=$(Robot_exec $subject RIGHT)
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" -eq 0 ]
+  [ "${values[2]}" == "EAST" ]
+}
+
+@test "Robot exec PLACE" {
+  local subject=$(Robot_new)
+  local subject=$(Robot_exec $subject PLACE 4,2,NORTH)
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 4 ]
+  [ "${values[1]}" -eq 2 ]
+  [ "${values[2]}" == "NORTH" ]
+}
+
+@test "Robot exec REPORT" {
+  local subject=$(Robot_new)
+  local subject=$(Robot_exec $subject REPORT)
+
+  [ "$subject" == "0,0,NORTH" ]
+
+  local values=(`echo $subject | tr ',' ' '`)
+
+  [ "${values[0]}" -eq 0 ]
+  [ "${values[1]}" -eq 0 ]
   [ "${values[2]}" == "NORTH" ]
 }
